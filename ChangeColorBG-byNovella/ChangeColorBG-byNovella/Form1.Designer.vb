8266e524652985dc4a10fa15cd19c0ad2ad7b04b
Novella Ayu Oktaviani <nevazilla@gmail.com>
2015-10-02 09:08:59 -0700

    Tugas 1 Ubah Warna
    Oleh Novella Ayu Oktaviani 1431140020

---
+++ ChangeColorBG-byNovella/ChangeColorBG-byNovella/Form1.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Change = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Change
+        '
+        Me.Change.Location = New System.Drawing.Point(197, 226)
+        Me.Change.Name = "Change"
+        Me.Change.Size = New System.Drawing.Size(75, 23)
+        Me.Change.TabIndex = 0
+        Me.Change.Text = "Change"
+        Me.Change.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.Change)
+        Me.Name = "Form1"
+        Me.Text = "NovellaForm"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Change As System.Windows.Forms.Button
+
+End Class
---
+++ ChangeColorBG-byNovella/ChangeColorBG-byNovella/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+
+    Private Sub Change_Click(sender As Object, e As EventArgs) Handles Change.Click
+        If Me.BackColor() = Color.LightPink Then
+            Me.BackColor() = Color.Aquamarine
+        Else
+            Me.BackColor() = Color.LightPink
+        End If
+    End Sub
+End Class
